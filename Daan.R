09a3a475cc55df8e01c304951b895b1d2e81e44e
Daan Hofman <daanhofman@outlook.com>
2022-05-28 15:07:17 +0200

    Extend to 1999

--- Daan.R
+++ Daan.R
@@ -11,10 +11,29 @@ library(ROCR)
 library(magrittr)
 
 ISS = read.csv2("data/daan/ISS.csv", head = TRUE, sep=",")
+ISSLegacy = read.csv2("data/daan/ISS legacy.csv", head = TRUE, sep=",")
 boardEx = read.csv("data/daan/boardex2.csv")
 execucomp = read.csv("data/daan/execucomp.csv") 
 ids = read.csv("data/char/ciq common.csv")
 fin = read.csv("data/daan/Fundamentals Daan.csv")
+
+#Append Legacy ISS data to new format
+ISSLegacy = ISSLegacy %>% 
+  rename(
+     year = YEAR,
+     Ticker = TICKER,
+     Name = NAME,
+     Employment_CEO = EMPLOYMENT_CEO,
+     Pcnt_Ctrl_Votingpower = PCNT_CTRL_VOTINGPOWER,
+     Num_Of_Shares = NUM_OF_SHARES,
+     Attend_LESS75_PCT = ATTEND_LESS75_PCT
+  )
+
+ISSLegacy$Employment_CEO = ifelse(ISSLegacy$Employment_CEO == 1, "Yes", "No")
+ISSLegacy$Attend_LESS75_PCT = ifelse(ISSLegacy$Attend_LESS75_PCT == 1, "Yes", "No")
+
+
+ISS = rbind(ISS, ISSLegacy)
 
 # Filters: Data Selection
 execucomp = execucomp[execucomp$CEOANN == "CEO", ]
@@ -50,7 +69,6 @@ df$genderRatio = rawData$GenderRatio
 df$industry = as.factor(substr(rawData$SPINDEX, 1, 2))
 df$OtherBoards = ifelse(is.na(rawData$TotNoOthLstdBrd), 0, rawData$TotNoOthLstdBrd)
 df$timeOtherComp = ifelse(is.na(rawData$AvgTimeOthCo), 0, rawData$AvgTimeOthCo)
-df$TimeUntilRetire = rawData$TimeRetirement
 df$year = rawData$YEAR
 df$sales =rawData$sale
 df$boardSize = rawData$NumberDirectors
@@ -62,7 +80,7 @@ hist(df$ceoDuality)
 
 # CEO Tenure
 df$ceoTenure <- rawData$YEAR - rawData$BECAMECEO
-df$ceoTenure2 = rawData$TimeRole
+#df$ceoTenure2 = rawData$TimeRole
 
 # CEO attendance
 df$ceoAttendance = ifelse(rawData$Attend_LESS75_PCT == "Yes", 1, 0)
@@ -73,6 +91,8 @@ df$ceoVotingPower = as.numeric(ifelse(is.na(rawData$Pcnt_Ctrl_Votingpower), 0, r
 # Bankruptcy score
 df$bankruptcy_score = 3.3 * rawData$ebit/rawData$at + 1 * rawData$sale/rawData$at + 0.6 * rawData$mkvalt/rawData$lt + 1.2 * rawData$wcap/rawData$at + 1.4 * rawData$re/rawData$at
 
+# --------------- Filters & transformations ----------------
+
 negativeTenure = df[df$ceoTenure < 0,]
 df = df[df$ceoTenure >= 0,]
 hist(df$ceoTenure)
@@ -80,17 +100,9 @@ summary(df$ceoTenure)
 
 df$ceoVotingPower[is.na(df$ceoVotingPower)] = 0
 
-summary(df)
 
 df = na.omit(df)
 
-hist(df$bankruptcy_score, breaks = 100)
-
-df = df[df$bankruptcy_score < 20]
-
-summary(df)
-
-df$bankruptcy_class = df$bankruptcy_score < 2.99
 
 
 #remove duplicate GVkey-year combinations
@@ -98,18 +110,32 @@ df = df %>%
   group_by(rawData.GVKEY, year) %>%
   slice_head(n=1)
 
+hist(df$bankruptcy_score, breaks = 100)
+
+
 
 dfCor = df
 dfCor$industry = NULL
 correlation = cor(dfCor, method = c("spearman"))
 stargazer(correlation)
 
+summary(df)
+
+# --------------- Plots ----------------
 ggplot(df, 
   aes(x=ceoTenure, y=bankruptcy_score)) +
   geom_point() + 
-  geom_smooth(method=glm , se=FALSE, fullrange=FALSE) +
-  ggtitle("Plot of average amount of aquisitions vs compensation ratio per board size") + 
+  geom_smooth(method=glm, formula = bankruptcy_score ~ ceoTenure + ceoTenure^2, se=FALSE, fullrange=FALSE) +
+  ggtitle("Plot of bankruptcy score vs CEO tenure") + 
   xlab("CEO charateristic") + ylab("Altman Z-score")
+
+#Check group size and remove all panels of size 1
+df = df %>%
+  group_by(rawData.GVKEY) %>%
+  mutate(freq = n()) %>% 
+  ungroup()
+
+df = df[df$freq > 1,]
 
 ###############################################
 #                                             #
@@ -122,60 +148,51 @@ mdlB = bankruptcy_score ~ ceoAge + ceoGender + firmSize + genderRatio + industry
 mdlC = bankruptcy_score ~ ceoAge + ceoGender + firmSize + genderRatio + industry + OtherBoards + boardSize + timeOtherComp + ceoTenure + ceoAttendance
 mdlD = bankruptcy_score ~ ceoAge + ceoGender + firmSize + genderRatio + industry + OtherBoards + boardSize + timeOtherComp + ceoTenure + ceoAttendance + ceoVotingPower
 mdlE = bankruptcy_score ~ ceoAge + ceoGender + firmSize + genderRatio + industry + OtherBoards + boardSize + timeOtherComp + ceoTenure + ceoAttendance + ceoVotingPower + ceoDuality
-mdlF = bankruptcy_score ~ ceoAge + ceoGender + firmSize + genderRatio + industry + OtherBoards + boardSize + ceoTenure + I(ceoTenure^2) + ceoAttendance + ceoVotingPower + ceoDuality
+mdlF = bankruptcy_score ~ ceoAge + ceoGender + firmSize + genderRatio + industry + OtherBoards + boardSize + timeOtherComp + ceoTenure + ceoAttendance + ceoVotingPower + ceoDuality + I(ceoTenure^2)
 
 
-df.p = pdata.frame(df, index=c("rawData.GVKEY", "year"))#Check group size
-df = df %>%
-  group_by(GVKEY) %>%
-  mutate(freq = n()) %>% 
-  ungroup()
-
-df = df[df$freq > 1,]
-summary(df.p)
-
-hist(df.p$ceoAge)
-hist(df.p$ceoGender)
-hist(df.p$genderRatio)
-hist(df.p$OtherBoards)
-hist(as.numeric(df.p$industry))
-hist(log(df.p$timeOtherComp))
-hist(log(df.p$ceoTenure))
-hist(df.p$ceoAttendance)
-hist(log(df.p$ceoVotingPower))
-hist(df.p$ceoDuality)
+df.p = pdata.frame(df, index=c("rawData.GVKEY", "year"))
 
 occur = data.frame(table(row.names(df.p)))
 duplicateRowNames = occur[occur$Freq > 1,]
+table(index(df.p), useNA = "ifany")
 
-#Check group size
-grouped = df.p %>%
+#Check group size and remove all panels of size 1
+df = df %>%
   group_by(rawData.GVKEY) %>%
-  summarise(n())
+  mutate(freq = n()) %>% 
+  ungroup()
 
+
+length(df$rawData.GVKEY)
+summary(df.p)
+stargazer(df.p, type = "latex", title="Descriptive statistics", digits=3)
 
 rsltA = plm(mdlA, df.p,  model="within")
 rsltB = plm(mdlB, df.p,  model="within")
 rsltC = plm(mdlC, df.p,  model="within")
 rsltD = plm(mdlD, df.p,  model="within")
 rsltE = plm(mdlE, df.p,  model="within")
+rsltF = plm(mdlF, df.p,  model="within")
 
 rsltERandom = plm(mdlE, df.p,  model="random")
 rsltEORS = plm(mdlE, df.p,  model="pooling")
+
+# Hausman test
 phtest(rsltE, rsltERandom)
 
 pFtest(rsltE, rsltEORS) 
 
 summary(rsltE)
-stargazer(rsltA, rsltB, rsltC, rsltD, rsltE, type = "latex")
-
-
+stargazer(rsltA, rsltB, rsltC, rsltD, rsltE, rsltF, type = "latex", add.lines = list(c("Fixed Effect: Year", "Yes", "Yes", "Yes", "Yes", "Yes"), c("Fixed Effect: Company", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes")))
 
 ###############################################
 #                                             #
 #            Machine Learning                 #
 #                                             #
 ###############################################
+df$bankruptcy_class = df$bankruptcy_score < 2.99
+
 
 mdlA = as.factor(bankruptcy_class) ~ ceoAge + ceoGender + firmSize + genderRatio + industry + OtherBoards + boardSize
 mdlB = as.factor(bankruptcy_class) ~ ceoAge + ceoGender + firmSize + genderRatio + industry + OtherBoards + boardSize + ceoTenure 
@@ -188,8 +205,10 @@ mdlE = as.factor(bankruptcy_class) ~ ceoAge + ceoGender + firmSize + genderRatio
 ## 75% of the sample size
 smp_size <- floor(0.75 * nrow(df))
 
+
 ## set the seed to make your partition reproducible
 set.seed(123)
+
 train_ind <- sample(seq_len(nrow(df)), size = smp_size)
 
 train <- df[train_ind, ]
@@ -275,11 +294,11 @@ summary(rsltGbmB)
 # ------------------- performance analysis ------------------
 
 yvalue    <- test$bankruptcy_class
-probTreeA <- predict(rsltTreeB, type="prob")[,2]
-probFrstA <- predict(rsltFrstB, type="prob")[,2]
-probGbmA  <- predict(rsltGbmB,  type="response", n.trees=nTrees)
-probReg   <- predict(rsltReg,   type="response")
-probLog   <- predict(rsltLog,   type="response")
+probTreeA <- predict(rsltTreeB, test, type="prob")[,2]
+probFrstA <- predict(rsltFrstB, test, type="prob")[,2]
+probGbmA  <- predict(rsltGbmB, test,  type="response", n.trees=nTrees)
+probReg   <- predict(rsltReg, test,   type="response")
+probLog   <- predict(rsltLog, test,   type="response")
 
 predTreeA <- as.numeric(probTreeA > 0.5)
 predFrstA <- as.numeric(probFrstA > 0.5)
@@ -288,7 +307,8 @@ predReg <-   as.numeric(probReg > 0.5)
 predLog <-   as.numeric(probLog > 0.5)
 
 # ---- II ----
-summary(yvalue)
+length(test)
+length(predTreeA)
 
 table(Predicted = predTreeA, Observed = yvalue)
 table(Predicted = predFrstA, Observed = yvalue)
@@ -308,7 +328,7 @@ perf.Reg    <- performance(pred.Reg,   measure="tpr", x.measure="fpr")
 perf.Log    <- performance(pred.Log,   measure="tpr", x.measure="fpr")
 
 
-plot(perf.TreeA, lty = 1, lwd = 2.0, col = "red", main="Classification performance")
+plot(perf.TreeA, lty = 1, lwd = 2.0, col = "red", main="Classification performance on test set")
 plot(perf.FrstA, lty = 1, lwd = 2.0, col = "blue", add = TRUE)
 plot(perf.GbmA,  lty = 1, lwd = 2.0, col = "green", add = TRUE)
 plot(perf.Reg,   lty = 1, lwd = 2.0, col = "gray", add = TRUE)
@@ -338,3 +358,4 @@ dsAUC <- data.frame(
 
 stargazer(dsAUC, summary = FALSE,
           align = TRUE, no.space = TRUE, rownames = FALSE)
+
